18b13160e07077990a5f97d90e26bf1d646eb620
최혜진 <choihyejin94@naver.com>
2024-12-23 19:12:16 +0900

    🔥 fix:package-lock.json삭제

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -19,17 +19,17 @@ export async function updateSession(request: NextRequest) {
 
   const { data: { user } } = await supabase.auth.getUser();
 
-  // 로그인 필요 페이지에 접근 시 비로그인 상태라면 /login으로 리다이렉트
-//   if (!user && !request.nextUrl.pathname.startsWith("/login")) {
-//     const url = request.nextUrl.clone();
-//     url.pathname = "/login";
-//     return NextResponse.redirect(url);
-//   }
+  // // 로그인 필요 페이지에 접근 시 비로그인 상태라면 /login으로 리다이렉트
+  // if (!user && !request.nextUrl.pathname.startsWith("/login")) {
+  //   const url = request.nextUrl.clone();
+  //   url.pathname = "/login";
+  //   return NextResponse.redirect(url);
+  // }
 
-//   // 로그인 상태인데 로그인 페이지로 가려는 경우 메인 페이지로 리다이렉트
-//   if (user && request.nextUrl.pathname.startsWith("/login")) {
-//     return NextResponse.redirect(request.nextUrl.origin);
-//   }
+  // // 로그인 상태인데 로그인 페이지로 가려는 경우 메인 페이지로 리다이렉트
+  // if (user && request.nextUrl.pathname.startsWith("/login")) {
+  //   return NextResponse.redirect(request.nextUrl.origin);
+  // }
 
   return NextResponse.next();
 }
