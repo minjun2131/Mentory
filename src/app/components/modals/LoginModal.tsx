cd19145dd49acf1eb9841fff8b090c8e594124d5
Raina-Moon <mds6425@gmail.com>
2024-12-25 02:31:44 +0900

    ✨ 로그인, 회원가입에 쓰이는 모달 생성

---
+++ src/app/components/modals/LoginModal.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ModalWrapper from './ModalWrapper';
+import { useModalStore } from '@/app/store/modalStore';
+import LoginForm from '@/app/auth/login/LoginForm';
+
+const LoginModal: React.FC = () => {
+  const { openModal, close } = useModalStore();
+
+  return (
+    <ModalWrapper isOpen={openModal === 'login'} onClose={close}>
+      <LoginForm />
+    </ModalWrapper>
+  );
+};
+
+export default LoginModal;
