bba8450d6b39052c04df44893069056add22b31f
Raina-Moon <mds6425@gmail.com>
2024-12-25 23:00:19 +0900

    💄 로그인,회원가입 모달 예쁘게 꾸며줌

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -1,35 +1,52 @@
 'use client';
 
 import React, { useState } from 'react';
+import Image from 'next/image';
+import { useModalStore } from '@/app/store/modalStore';
 
 const LoginForm: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { open } = useModalStore();
 
   const handleLogin = () => {
     console.log('로그인이 되는가!!!!');
   };
 
   return (
-    <div>
-      <h2>Log In</h2>
-      <input
-        type="email"
-        placeholder="이메일을 입력하세요"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className="border p-2 w-full rounded mb-4"
-      />
-      <input
-        type="password"
-        placeholder="비밀번호를 입력하세요"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className="border p-2 w-full rounded mb-4"
-      />
-      <button onClick={handleLogin} className="bg-main text-white px-4 py-2 rounded w-full hover:bg-main-hover">
-        Log In
-      </button>
+    <div className="p-6">
+      <div className="text-center mb-6">
+        <Image src="/images/logo.png" alt="Mentory_Logo" width={300} height={70} className="mx-auto" />
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold text-center mb-4">로그인</h2>
+        <input
+          type="email"
+          placeholder="아이디"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border p-2 w-full rounded mb-4 bg-gray-200 placeholder-black"
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border p-2 w-full rounded mb-4 bg-gray-200 placeholder-black"
+        />
+        <button
+          onClick={handleLogin}
+          className="bg-main text-white py-2 px-8 min-w-[200px] rounded mx-auto block hover:bg-main-hover transition-all"
+        >
+          로그인
+        </button>
+        <span className="block text-center mt-4">
+          계정이 없으신가요?
+          <button onClick={() => open('signup')} className="text-main hover:underline">
+            회원가입
+          </button>
+        </span>
+      </div>
     </div>
   );
 };
--- src/app/auth/signup/SignupForm.tsx
+++ src/app/auth/signup/SignupForm.tsx
@@ -1,39 +1,68 @@
 'use client';
 
 import React, { useState } from 'react';
+import Image from 'next/image';
+import { useModalStore } from '@/app/store/modalStore';
 
 const SignupForm: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { open } = useModalStore();
 
   const handleSignup = () => {
     console.log('회원가입이 되는가!!!!');
   };
 
   return (
-    <div>
-      <h2>Log In</h2>
-      <input
-        type="email"
-        placeholder="이메일을 입력하세요"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className="border p-2 w-full rounded mb-4"
-      />
-      <input
-        type="password"
-        placeholder="비밀번호를 입력하세요"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className="border p-2 w-full rounded mb-4"
-      />
-      <button onClick={handleSignup} className="bg-main text-white px-4 py-2 rounded w-full hover:bg-main-hover">
-        Sign In
-      </button>
+    <div className="p-6">
+      <div className="text-center mb-6">
+        <Image src="/images/logo.png" alt="Mentory_Logo" width={300} height={70} className="mx-auto" />
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold text-center mb-4">회원가입</h2>
+        <input
+          type="email"
+          placeholder="이메일"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border p-2 w-full rounded mb-4 bg-gray-200 placeholder-black"
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border p-2 w-full rounded mb-4 bg-gray-200 placeholder-black"
+        />
+        <input
+          type="password"
+          placeholder="비밀번호 확인"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border p-2 w-full rounded mb-4 bg-gray-200 placeholder-black"
+        />
+        <input
+          type="name"
+          placeholder="이름"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border p-2 w-full rounded mb-4 bg-gray-200 placeholder-black"
+        />
+        <button
+          onClick={handleSignup}
+          className="bg-main text-white py-2 px-8 min-w-[200px] rounded mx-auto block hover:bg-main-hover transition-all"
+        >
+          회원가입
+        </button>
+        <span className="block text-center mt-4">
+          이미 가입하셨나요?
+          <button onClick={() => open('login')} className="text-main hover:underline">
+            로그인
+          </button>
+        </span>
+      </div>
     </div>
   );
 };
 
 export default SignupForm;
-
-
