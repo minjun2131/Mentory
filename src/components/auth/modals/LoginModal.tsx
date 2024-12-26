a39b375c8f9a223b67b43385f7ae2a8668d360c0
Raina-Moon <mds6425@gmail.com>
2024-12-26 17:39:01 +0900

    ✨ 깃헙 소셜 로그인 구현 로직들

--- src/components/auth/modals/LoginModal.tsx
+++ src/components/auth/modals/LoginModal.tsx
@@ -4,13 +4,17 @@ import React from 'react';
 import ModalWrapper from '../../ui/ModalWrapper';
 import { useModalStore } from '@/store/modalStore';
 import LoginForm from '@/components/auth/forms/LoginForm';
+import GitHubLoginButton from '../buttons/GitHubLoginButton';
 
 const LoginModal: React.FC = () => {
   const { openModal, close } = useModalStore();
 
   return (
     <ModalWrapper isOpen={openModal === 'login'} onClose={close}>
+      <div className='"space-y-4"'>
       <LoginForm />
+      <GitHubLoginButton />
+      </div>
     </ModalWrapper>
   );
 };
