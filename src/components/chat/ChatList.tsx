27978fe81ac94458128e11f980666c8c6eb4c15a
ovezen <klimen1005@gmail.com>
2024-12-27 14:39:54 +0900

    💄 채팅ui 변경

--- src/components/chat/ChatList.tsx
+++ src/components/chat/ChatList.tsx
@@ -2,13 +2,16 @@
 
 import { Database } from '@/types/supabase';
 import { createClient } from '@/utils/supabase/client';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 type ChatRoom = Database['public']['Tables']['chatrooms']['Row'];
+type User = Database['public']['Tables']['users']['Row'];
 
 const ChatList = ({ onSelectChatroom }: { onSelectChatroom: (id: string, userId: string) => void }) => {
   const [chatRooms, setChatRooms] = useState<ChatRoom[]>([]);
   const [userId, setUserId] = useState<string | null>(null);
+  const [users, setUsers] = useState<User[]>([]);
   const supabase = createClient();
 
   // 채팅방 목록 조회
@@ -45,20 +48,64 @@ const ChatList = ({ onSelectChatroom }: { onSelectChatroom: (id: string, userId:
       } else {
         setChatRooms(data);
       }
+
+      // 사용자 정보 조회
+      const { data: userData } = await supabase.from('users').select('*');
+      if (!userData) {
+        console.error('사용자 정보 불러오기 실패');
+      } else {
+        setUsers(userData);
+      }
     };
 
     fetchChatRooms();
   }, [supabase]);
 
+  //채팅방 이름 생성
+  const getChatRoomName = (room: ChatRoom) => {
+    const mentor = users.find((user) => user.id === room.mentor_id);
+    const mentee = users.find((user) => user.id === room.mentee_id);
+
+    // 현재 사용자가 멘토인 경우
+    if (mentor && mentor.id === room.mentor_id) {
+      return {
+        profileImage: mentee?.profile_image,
+        name: `${mentee?.name}님과의 채팅방`
+      };
+    }
+    // 현재 사용자가 멘티인 경우
+    else if (mentee && mentee.id === room.mentee_id) {
+      return {
+        profileImage: mentee?.profile_image,
+        name: `${mentor?.name}님과의 채팅방`
+      };
+    }
+    return { profileImage: null, name: '채팅방' };
+  };
+
   return (
     <div className="p-4">
-      <h3 className="text-lg font-semibold mb-4">채팅 리스트</h3>
       <ul>
-        {chatRooms.map((room) => (
+        {chatRooms.map((room) => {
+          const { name, profileImage } = getChatRoomName(room);
+          return (
+            <li
+              key={room.id}
+              onClick={() => userId && onSelectChatroom(room.id, userId)}
+              className="bg-gray-200 border border-gray-300 rounded-lg p-3 mb-2 cursor-pointer transition duration-300 hover:bg-gray-300 flex items-center"
+            >
+              {profileImage && (
+                <Image src={profileImage} alt="Profile" width={40} height={0} className="rounded-full mr-3" />
+              )}
+              <span>{name}</span>
+            </li>
+          );
+        })}
+        {/* {chatRooms.map((room) => (
           <li key={room.id} onClick={() => userId && onSelectChatroom(room.id, userId)}>
             {room.id}
           </li>
-        ))}
+        ))} */}
       </ul>
     </div>
   );
--- src/components/chat/ChatRoom.tsx
+++ src/components/chat/ChatRoom.tsx
@@ -61,12 +61,21 @@ const ChatRoom = ({ chatroomId, userId }: { chatroomId: string | null; userId: s
     }
   };
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      handleSendMessage();
+    }
+  };
+
   return (
     <div className="p-4 flex flex-col h-full">
       <h3 className="text-lg font-semibold mb-4">채팅방</h3>
       <div className="flex-1 overflow-y-auto">
         {messages.map((message) => (
-          <div key={message.id}>{message.content}</div>
+          <div key={message.id} className="p-2 mb-2 rounded-lg bg-blue-500 text-white self-end">
+            {message.content}
+          </div>
         ))}
       </div>
       <div className="flex items-center p-2 border-t">
@@ -74,13 +83,11 @@ const ChatRoom = ({ chatroomId, userId }: { chatroomId: string | null; userId: s
           type="text"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={handleKeyDown}
           placeholder="메시지를 입력하세요..."
           className="flex-1 px-4 py-2 border rounded-lg focus:outline-none"
         />
-        <button
-          onClick={handleSendMessage}
-          className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg focus:outline-none"
-        >
+        <button onClick={handleSendMessage} className="ml-2 px-4 py-2 bg-main text-white rounded-lg focus:outline-none">
           전송
         </button>
       </div>
